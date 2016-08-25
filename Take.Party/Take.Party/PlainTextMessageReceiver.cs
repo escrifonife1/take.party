b77bed15fedf474e04309a3da4bfbfe582738701
tiago <tiagod@take.net>
2016-08-25 19:07:47 -0300

    Try to get token

--- Take.Party/Take.Party/PlainTextMessageReceiver.cs
+++ Take.Party/Take.Party/PlainTextMessageReceiver.cs
@@ -8,6 +8,7 @@ using Takenet.MessagingHub.Client.Sender;
 using SpotifyAPI.Web.Models;
 using SpotifyAPI.Web.Enums;
 using System.Linq;
+using SpotifyAPI.Web.Auth;
 
 namespace Take.Party
 {
@@ -22,9 +23,12 @@ namespace Take.Party
 
         public async Task ReceiveAsync(Message message, CancellationToken cancellationToken)
         {
+            //EXEMPLO ESTA AQUI http://johnnycrazy.github.io/SpotifyAPI-NET/SpotifyWebAPI/auth/
+            await GetToken(); //ESSA LINHA É SO PARA PEGAR O TOKEN. DEPOIS DISSO PODE SER COMENTADA
+                        
             var _spotify = new SpotifyAPI.Web.SpotifyWebAPI();
-            _spotify.AccessToken = "";
-            var item = _spotify.SearchItems(message.Content.ToString(), SearchType.Track);
+            _spotify.AccessToken = "COLOCAR O TOKEN GERADO AQUI";
+                var item = _spotify.SearchItems(message.Content.ToString(), SearchType.Track);
             var tracks = "Não achou";
             if (item.Tracks != null)
             {
@@ -32,6 +36,18 @@ namespace Take.Party
             }
             Console.WriteLine($"From: {message.From} \tContent: {message.Content}");
             await _sender.SendMessageAsync(tracks, message.From, cancellationToken);
+        }
+
+        private async Task GetToken()
+        {
+            WebAPIFactory webApiFactory = new WebAPIFactory(
+                   "http://requestb.in/176gbee1",
+                   8000,
+                   "6371ccb6dc9e44069bca6763bcb539b6",
+                   Scope.UserReadPrivate,
+                   TimeSpan.FromSeconds(20)
+              );
+            var _spotify = await webApiFactory.GetWebApi();
         }
     }
 }
